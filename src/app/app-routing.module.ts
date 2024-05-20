6c8991cf8ca1e6328b258308a1ead20c8c96d6ac
awedga <x@gmail.com>
2024-05-20 16:00:44 -0500

    primer commit
    subo todos los archivos

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AnimalComponent } from './components/animal/animal.component';
+import { SignupComponent } from './authentication/signup/signup.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+
+const routes: Routes = [
+  {path: '',component: SignupComponent},
+  {path: 'animal',component: AnimalComponent},
+  {path: 'signup',component: SignupComponent},
+  {path: 'login',component: LoginComponent},
+  {path: 'logout',component: AppComponent},
+  {path: 'menu',component: MenuComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
